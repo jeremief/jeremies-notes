d1fc7a395895cd28b8264ce9d493e843105248c8
jeremief <quasimobeau@yahoo.fr>
2016-01-28 17:07:45 +1100

    main.py is now PEP8 compliant

--- main.py
+++ main.py
@@ -52,64 +52,90 @@ def swear_check(swear_check_text):
         final_text = swear_check_text
     return final_text
 
+
 def curly_cleaner(raw_string):
+    """
+    Clearing curly brackets and their contents
+    """
     end_flag = False
-    while raw_string.find('{{')!= -1 and end_flag == False:
+    while raw_string.find('{{') != -1 and end_flag is False:
         start_curly = raw_string.find('{{')
-        if raw_string.find('}}', start_curly)!=-1:
-            end_curly = raw_string.find('}}',start_curly)+ 2
+        if raw_string.find('}}', start_curly) != -1:
+            end_curly = raw_string.find('}}', start_curly) + 2
             raw_string = raw_string[:start_curly] + raw_string[end_curly:]
         else:
             end_flag = True
     return raw_string
 
+
 def ref_cleaner(raw_string):
+    """
+    Clearing reference tags and their contents
+    """
     end_flag = False
-    while raw_string.find('<ref')!= -1 and end_flag == False:
+    while raw_string.find('<ref') != -1 and end_flag is False:
         start_ref = raw_string.find('<ref')
-        if raw_string.find('</ref>', start_ref)!=-1:
-            end_ref = raw_string.find('</ref>',start_ref)+ 6
+        if raw_string.find('</ref>', start_ref) != -1:
+            end_ref = raw_string.find('</ref>', start_ref) + 6
             raw_string = raw_string[:start_ref] + raw_string[end_ref:]
         else:
             end_flag = True
     return raw_string
 
+
 def pipe_cleaner(raw_string):
+    """
+    Removing pipes and the contents to their left
+    """
     end_flag = False
-    while raw_string.find('|') != -1 and end_flag == False:
+    while raw_string.find('|') != -1 and end_flag is False:
         pipe = raw_string.find('|')
-        begining_word = raw_string.rfind('[',0, pipe)
-        raw_string = raw_string[:begining_word+1] + raw_string[pipe + 1:]
+        begining_word = raw_string.rfind('[', 0, pipe)
+        raw_string = raw_string[:begining_word + 1] + raw_string[pipe + 1:]
     return raw_string
 
+
 def square_cleaner(raw_string):
+    """
+    Clearing square brackets
+    """
     end_flag = False
-    while raw_string.find('[[') != -1 and end_flag == False:
+    while raw_string.find('[[') != -1 and end_flag is False:
         start_square = raw_string.find('[[')
-        if raw_string.find(']]', start_square)!=-1:
+        if raw_string.find(']]', start_square) != -1:
             end_square = raw_string.find(']]', start_square)
-            raw_string = raw_string[:start_square] + raw_string[start_square+2 : \
-            end_square] + raw_string[end_square+2:]
+            raw_string = raw_string[:start_square] + \
+                raw_string[start_square + 2:
+                           end_square] + raw_string[end_square + 2:]
         else:
             end_flag = True
     return raw_string
 
+
 def note_cleaner(raw_string):
+    """
+    Clearing note tags and their contents
+    """
     end_flag = False
-    while raw_string.find('<!--') != -1 and end_flag == False:
+    while raw_string.find('<!--') != -1 and end_flag is False:
         start_note = raw_string.find('<!--')
-        if raw_string.find('-->', start_note)!=-1:
+        if raw_string.find('-->', start_note) != -1:
             end_note = raw_string.find('-->', start_note) + 3
             raw_string = raw_string[:start_note] + raw_string[end_note:]
         else:
             end_flag = True
     return raw_string
 
+
 def various_cleaner(raw_string):
-    raw_string = raw_string.replace('&nbsp;',' ')
-    raw_string = raw_string.replace('(; ','(')
-    raw_string = raw_string.replace('(; ; ','(')
+    """
+    Clearing various characters left after various cleanups
+    """
+    raw_string = raw_string.replace('&nbsp;', ' ')
+    raw_string = raw_string.replace('(; ', '(')
+    raw_string = raw_string.replace('(; ; ', '(')
     return raw_string
+
 
 def text_cleaner(raw_text):
     curly_cleaned_text = curly_cleaner(raw_text)
@@ -121,6 +147,7 @@ def text_cleaner(raw_text):
     cleaned_text = cleaned_text.decode('unicode-escape')
     cleaned_text = cleaned_text.encode('utf-8')
     return cleaned_text
+
 
 class SummaryClass(ndb.Model):
     """A model for a Wikipedia summary."""
@@ -172,11 +199,11 @@ class FourPage(Handler):
 
 
 class FivePage(Handler):
-    def get(self,*summary):
+    def get(self, *summary):
 
         number_of_summaries_to_fetch = 1
-        summary_query = SummaryClass.query(ancestor = summary_key()).\
-        order(-SummaryClass.time_request)
+        summary_query = SummaryClass.query(ancestor=summary_key()).\
+            order(-SummaryClass.time_request)
         summary_list = summary_query.fetch(number_of_summaries_to_fetch)
         if summary_list:
             summary = summary_list[0]
@@ -184,7 +211,7 @@ class FivePage(Handler):
             summary_link = str(summary.wlink)
 
             template_values = {'summary': summary_content,
-                               'wlink': summary_link,}
+                               'wlink': summary_link, }
 
             template = jinja_env.get_template('stagefive.html')
             self.response.write(template.render(template_values))
@@ -193,7 +220,6 @@ class FivePage(Handler):
 
 
 class ApiExemple(Handler):
-
 
     def post(self):
 
@@ -205,20 +231,21 @@ class ApiExemple(Handler):
 
         if user_entry != "":
 
-            while redirect_flag == False:
-                #Extract text from URL
+            while redirect_flag is False:
+                # Extract text from URL
                 user_entry = user_entry.title()
                 modified_user_entry = user_entry.replace(" ", "_")
                 print modified_user_entry
-                url_for_api = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + \
-                modified_user_entry +'&prop=revisions&rvprop=content&format=json'
+                url_for_api = ('https://en.wikipedia.org/w/api.php?action=' +
+                               'query&titles=' + modified_user_entry +
+                               '&prop=revisions&rvprop=content&format=json')
                 response = urllib2.urlopen(url_for_api)
                 res = response.read()
                 response.close()
 
                 # Find page ID
                 start_pageid = res.find("pages") + num_characters_before_id
-                end_pageid = res.find('"',start_pageid)
+                end_pageid = res.find('"', start_pageid)
                 page_id = res[start_pageid:end_pageid]
                 page_id = int(page_id)
 
@@ -230,47 +257,42 @@ class ApiExemple(Handler):
                     redirect_flag = True
                     start_redirect = res.find("REDIRECT")
                     if start_redirect != -1:
-                        first_curly_before_redirect = res.find('[',start_redirect)
+                        first_curly_before_redirect = res.find('[',
+                                                               start_redirect)
                         num_characters_before_redirect = \
-                        first_curly_before_redirect - start_redirect + 2
-                        end_redirect = res.find(']]',start_redirect)
+                            first_curly_before_redirect - start_redirect + 2
+                        end_redirect = res.find(']]', start_redirect)
                         redirect = res[start_redirect +
                                        num_characters_before_redirect:
                                        end_redirect]
                         user_entry = redirect
                         redirect_flag = False
 
-        MySummary = SummaryClass(parent = summary_key())
+        MySummary = SummaryClass(parent=summary_key())
 
         if user_entry != "":
 
-            if page_id !=-1:
+            if page_id != -1:
                 start_summary = res.find("'''")
                 end_summary = res.find("==")
 
-                if start_summary != -1 and start_summary <end_summary:
-
+                if start_summary != -1 and start_summary < end_summary:
                     raw_summary = res[start_summary:end_summary]
-
                     raw_summary = text_cleaner(raw_summary)
-
-
                     MySummary.content = raw_summary
-                    MySummary.wlink = 'http://en.wikipedia.org/?curid='+ \
-                    str(page_id)
+                    MySummary.wlink = 'http://en.wikipedia.org/?curid=' + \
+                        str(page_id)
                     MySummary.put()
-
                     self.redirect('/five#summary')
 
                 else:
-                    MySummary.content = "Unfortunately, we couldn't generate a \
-                    summary for this page. Please follow the link below to find \
-                    out more."
-                    MySummary.wlink = 'http://en.wikipedia.org/?curid='+ \
-                    str(page_id)
+                    MySummary.content = "Unfortunately, we couldn't generate \
+                    a summary for this page. Please follow the link below to \
+                    find out more."
+                    MySummary.wlink = 'http://en.wikipedia.org/?curid=' + \
+                                      str(page_id)
                     MySummary.put()
                     self.redirect('/five#summary')
-
 
             else:
                 MySummary.content = "No such page referenced by Wikipedia \
@@ -288,16 +310,10 @@ class ApiExemple(Handler):
             self.redirect('five#summary')
 
 
-
-
-
-
 class Comment(ndb.Model):
     """A main model for representing an individual comment."""
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
-
-
 
 
 class CommentsPage(webapp2.RequestHandler):
@@ -305,7 +321,6 @@ class CommentsPage(webapp2.RequestHandler):
 
         error = self.request.get('error', '')
         number_of_records_to_fetch = 20
-
 
         comments_query = Comment.query(
             ancestor=comment_wall_key()).order(-Comment.date)
@@ -334,11 +349,11 @@ class CommentsPage(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([('/', HomePage),
                                ('/zero', ZeroPage),
-                               ('/one',OnePage),
-                               ('/two',TwoPage),
-                               ('/three',ThreePage),
-                               ('/four',FourPage),
-                               ('/five',FivePage),
-                               ('/api',ApiExemple),
-                               ('/comments',CommentsPage)],
+                               ('/one', OnePage),
+                               ('/two', TwoPage),
+                               ('/three', ThreePage),
+                               ('/four', FourPage),
+                               ('/five', FivePage),
+                               ('/api', ApiExemple),
+                               ('/comments', CommentsPage)],
                               debug=True)

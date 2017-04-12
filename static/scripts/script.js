31c2b519d670e5281b243dd8e337028f6c5c8b9e
jeremief <quasimobeau@yahoo.fr>
2017-04-13 08:42:07 +1000

    Refactor: move the nightmode script to a seperate js file

---
+++ static/scripts/script.js
@@ -0,0 +1,20 @@
+function nightmode() {
+
+    $("body").toggleClass("nightmode-body");
+    $("p").toggleClass("nightmode-p");
+    $("h2").toggleClass("nightmode-h2");
+    $(".stage").toggleClass("nightmode-stage");
+    $(".topic").toggleClass("nightmode-topic");
+    $(".menu").toggleClass("nightmode-menu");
+    $(".point").toggleClass("nightmode-point");
+    $(".intro-text").toggleClass("nightmode-intro-text");
+    $(".summary").toggleClass("nightmode-summary");
+    $(".code").toggleClass("nightmode-code");
+    $(".comment").toggleClass("nightmode-comment");
+    $(".text-container").toggleClass("nightmode-text-container");
+    $(".menu-link").toggleClass("nightmode-menu-link");
+    $(".button-container").toggleClass("nightmode-button-container");
+    $(".night-day-button").toggleClass("nightmode-night-day-button");
+    $("a").toggleClass("nightmode-normal-link");
+}
+
